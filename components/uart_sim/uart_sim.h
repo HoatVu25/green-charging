0e4f547d4941a88239def7ea1ea6f9010f002c00
HoatVu25 <vuhuuhoat123@gmail.com>
2026-04-07 18:37:44 +0700

    Hoat_update_uart_sim

--- components/uart_sim/uart_sim.h
+++ components/uart_sim/uart_sim.h
@@ -9,7 +9,6 @@
 
 #define MQTT_CLIENT_ID   "GREEN_CHARGING"
 #define MQTT_BROKER_URI  "tcp://103.75.187.153:1885"
-#define MQTT_KEEPALIVE   60
 #define MQTT_SUB_TOPIC   "GC1765606666"
 #define HTTP_URL  "https://api.kullhi.id.vn/charging-post/confirm-start-charging"
 
@@ -20,4 +19,15 @@ bool mqtt_sub_start(const char *topic, int timeout_ms);
 bool mqtt_listen(char *data, int timeout);
 bool http_confirm_start_charging(const char *data);
 
-
+typedef enum {
+    EVT_SIM_READY,
+    EVT_SIM_SIGNAL_OK,
+    EVT_GPRS_CONNECTED,
+    EVT_MQTT_CONNECTED,
+    EVT_MQTT_MESSAGE,
+    EVT_NETWORK_LOST,
+    EVT_HTTP_CONNECTED,
+    EVT_RETRY,
+    EVT_GET_MESSAGE_MQTT,
+    EVT_POST_HTTP
+} system_event_t;
